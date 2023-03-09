2a4a0098cb698d89e5b1bc1187f65a864cf81952
JG-OLIVEIRA <jorge.gdoliveira8@gmail.com>
2023-03-09 13:08:23 -0300

    inter service communication between order and inventory service

---
+++ inventory-service/src/main/java/com/programming/techie/orderservice/services/InventoryService.java
@@ -0,0 +1,31 @@
+package com.programming.techie.orderservice.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.programming.techie.orderservice.repository.InventoryRepository;
+import com.programming.techie.orderservice.dto.InventoryResponse;
+
+import lombok.RequiredArgsConstructor;
+
+@Service
+@RequiredArgsConstructor
+public class InventoryService {
+
+    private final InventoryRepository inventoryRepository;
+
+    @Transactional(readOnly = true)
+    public List<InventoryResponse> isInStock(List<String> skuCode){
+        return inventoryRepository.findBySkuCodeIn(skuCode).stream()
+                .map(inventory ->
+                    InventoryResponse.builder()
+                            .skuCode(inventory.getSkuCode())
+                            .isInStock(inventory.getQuantity() > 0)
+                            .build()
+                ).collect(Collectors.toList());
+    }
+
+}
---
+++ inventory-service/src/test/java/com/programming/techie/orderservice/InventoryServiceApplicationTests.java
@@ -0,0 +1,12 @@
+package com.programming.techie.orderservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InventoryServiceApplicationTests {
+
+	@Test
+	void contextLoads() {}
+
+}
